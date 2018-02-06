06869e3045b49ff148068d30926750311211cc19
Zong-Ze <r05921012@ntu.edu.tw>
2018-02-06 14:59:00 +0800

    data_revise and add eos

--- Emotion_classification/dataset/crowdflower/data_generation.py
+++ Emotion_classification/dataset/crowdflower/data_generation.py
@@ -79,10 +79,10 @@ def main():
 	print('max len : ', max_len)
 	print(data[1])
 	X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.1, random_state=42)
-	np.save('train_data/x_train.npy', X_train)
-	np.save('train_data/y_train.npy', y_train)
-	np.save('train_data/X_test.npy', X_test)
-	np.save('train_data/y_test.npy', y_test)
+	# np.save('train_data/x_train.npy', X_train)
+	# np.save('train_data/y_train.npy', y_train)
+	# np.save('train_data/X_test.npy', X_test)
+	# np.save('train_data/y_test.npy', y_test)
 	# Countvectorizer
 
 	vectorizer = CountVectorizer(tokenizer=TB().tokenize, min_df = 4)
@@ -92,12 +92,53 @@ def main():
 	#inverse = [(value, key) for key, value in vectorizer.vocabulary_.items()]
 	max_voc_size = len(vectorizer.vocabulary_)
 	vectorizer.vocabulary_["unk"] = max_voc_size
+	vectorizer.vocabulary_["eos"] = max_voc_size + 1
 	max_voc_size =  len(vectorizer.vocabulary_)
 	print(max_voc_size)
 	inv_map = {v: k for k, v in vectorizer.vocabulary_.items()}
 	np.save('train_data/voc_dict.npy', vectorizer.vocabulary_)
 	np.save('train_data/inv_voc_dict.npy', inv_map)
 	training_data = []
+
+	for i in range(len(data)):
+		sentence_buf = []
+		#it = 0
+		for j in range(len(data[i])):
+			#print(data[i])
+			#print(TB().tokenize(data[i][j]))
+			buf = TB().tokenize(data[i][j])
+			#print(buf)
+			for k in range(len(buf)):
+				#it += 1
+				if buf[k].lower() in vectorizer.stop_words_:
+					buf[k] = "unk"
+				else:
+					buf[k] = buf[k].lower()
+				#print(buf[k])
+				sentence_buf.append(buf[k])
+		#print(it)				
+		#padddddddddding
+		if len(sentence_buf) < MAX_LEN_SEQUENCE:
+			pad_zero = ["eos"]*(MAX_LEN_SEQUENCE-len(sentence_buf))
+			sentence_buf = sentence_buf + pad_zero
+		if len(sentence_buf) != MAX_LEN_SEQUENCE:
+			#print(pad_zero)
+			print(len(sentence_buf))
+			print(data[i])
+			print(len(data[i]))
+			print('errrrrrrr!!!')
+			break
+		#print(len(sentence_buf))
+		#print(sentence_buf)
+		training_data.append(sentence_buf)	
+		#print(training_data)
+	print(len(training_data))
+	X_train, X_test, y_train, y_test = train_test_split(training_data, label, test_size=0.1, random_state=42)
+	np.save('train_data/x_train.npy', X_train)
+	np.save('train_data/y_train.npy', y_train)
+	np.save('train_data/X_test.npy', X_test)
+	np.save('train_data/y_test.npy', y_test)
+
 	#if 'fridaaaayyyyy' in vectorizer.stop_words_:
 	#	print('lichen fuck')
 	#print(vectorizer.stop_words_)
